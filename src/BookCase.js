bc6a00c554049df53e47111396d44bc930f4042d
Chris Boutiette <cboutiette@shelltechworks.com>
2017-12-22 01:28:32 -0500

    feat: ADD component for the book case

---
+++ src/BookCase.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+/**
+ * BookCase will be the area that contains all of the book shelves
+ * in addition to books.
+ */
+class BookCase extends Component
+{
+    // ProtoTypes here
+
+    // Render method here
+}
+
+export default BookCase
